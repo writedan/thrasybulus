cb9e6893803bb14b658512e7414ccbe23e580553
Daniel Write <daniel@writefamily.com>
2024-10-16 21:21:06 -0400

    added database capabilities

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -24,6 +24,34 @@ struct Cli {
 }
 
 fn main() {
+    match entry() {
+        Ok(_) => println!("{} Exited without error.", "Warning!".yellow().bold()),
+        Err(why) => {
+            println!("{}: {}", "Error".red().bold(), why);
+            std::process::exit(1);
+        }
+    }
+}
+
+/// Helper method to convert SystemTime to rusqlite timestamps
+fn system_time_to_date_time(t: std::time::SystemTime) -> chrono::DateTime<chrono::Utc> {
+    use chrono::TimeZone;
+    let (sec, nsec) = match t.duration_since(std::time::UNIX_EPOCH) {
+        Ok(dur) => (dur.as_secs() as i64, dur.subsec_nanos()),
+        Err(e) => { // unlikely but should be handled
+            let dur = e.duration();
+            let (sec, nsec) = (dur.as_secs() as i64, dur.subsec_nanos());
+            if nsec == 0 {
+                (-sec, 0)
+            } else {
+                (-sec - 1, 1_000_000_000 - nsec)
+            }
+        },
+    };
+    chrono::Utc.timestamp(sec, nsec)
+}
+
+fn entry() -> Result<(), String> {
     use pnet::datalink::{self, Channel};
     use pnet::packet::Packet;
     use pnet::packet::ethernet::{EthernetPacket, EtherType};
@@ -37,8 +65,7 @@ fn main() {
     }).next();
 
     if let None = interface {
-        println!("{}: No such interface: {}", "Error".red().bold(), cli.interface.bright_black().bold());
-        std::process::exit(1);
+        return Err(format!("No such interface: {}", cli.interface.bright_black().bold()));
     }
 
     let interface = interface.unwrap();
@@ -51,13 +78,30 @@ fn main() {
         Ok(Channel::Ethernet(tx, rx)) => (tx, rx),
 
         Ok(_) => {
-            println!("{}: Unsupported channel type", "Error".red().bold());
-            std::process::exit(2);
+            return Err("Unsupported channel type.".into());
         },
 
-        Err(err) => {
-            println!("{}: Failed to initialize channel: {}", "Error".red().bold(), err.to_string().bright_black().bold());
-            std::process::exit(3);
+        Err(why) => {
+            return Err(format!("Failed to initialize channel: {}", why.to_string().bright_black().bold()));
+        }
+    };
+
+    let database = match rusqlite::Connection::open(format!("{}.db", cli.interface)) {
+        Ok(db) => db,
+        Err(why) => return Err(format!("Failed to initialize database: {}", why.to_string().bright_black().bold()))
+    };
+
+    match database.execute("CREATE TABLE IF NOT EXISTS arp_requests(id INTEGER PRIMARY KEY, source TEXT, dest TEXT, timestamp INTEGER);", ()) {
+        Ok(_) => {},
+        Err(why) => {
+            return Err(format!("Failed to execute query: {}", why.to_string().bright_black().bold()));
+        }
+    };
+
+    match database.execute("CREATE TABLE IF NOT EXISTS arp_replies(id INTEGER PRIMARY KEY, replier_ip TEXT, replier_mac TEXT, destination_ip TEXT, timestamp INTEGER)", ()) {
+        Ok(_) => {},
+        Err(why) => {
+            return Err(format!("Failed to execute query: {}", why.to_string().bright_black().bold()));
         }
     };
 
@@ -66,11 +110,41 @@ fn main() {
     loop {
         match rx.next() {
             Ok(packet) => {
-                let eth_packet = EthernetPacket::new(packet).unwrap();
+                let eth_packet = match EthernetPacket::new(packet) {
+                    Some(p) => p,
+                    None => {
+                        return Err("No packet to read.".into());
+                    }
+                };
+
                 if eth_packet.get_ethertype() == EtherType(0x0806) {
                     if let Some(arp_packet) = ArpPacket::new(eth_packet.payload()) {
-                        if let pnet::packet::arp::ArpOperation(2) = arp_packet.get_operation() {
-                            println!("{} {} is at {} (destination: {})", "ARP REPLY".blue(),arp_packet.get_sender_proto_addr(), arp_packet.get_sender_hw_addr(), arp_packet.get_target_proto_addr());
+                        let pnet::packet::arp::ArpOperation(opcode) = arp_packet.get_operation();
+                        if opcode == 1 {
+                            // ARP REQUEST
+                            let dest = arp_packet.get_target_proto_addr().to_string();
+                            let source = arp_packet.get_sender_proto_addr().to_string();
+                            let timestamp = system_time_to_date_time(std::time::SystemTime::now());
+                            match database.execute("INSERT INTO arp_requests (source, dest, timestamp) VALUES(?1, ?2, ?3)", (&source, &dest, &timestamp)) {
+                                Ok(_) => {},
+                                Err(why) => {
+                                    return Err(format!("Failed to execute query: {}", why.to_string().bright_black().bold()));
+                                }
+                            };
+                            println!("{} {} asks for {}", "ARP REQUEST".blue(), source.bright_black().bold(), dest.bright_black().bold());
+                        } else if opcode == 2 {
+                            // ARP REPLY
+                            let replier_ip = arp_packet.get_sender_proto_addr().to_string();
+                            let replier_mac = arp_packet.get_sender_hw_addr().to_string();
+                            let destination_ip = arp_packet.get_target_proto_addr().to_string();
+                            let timestamp = system_time_to_date_time(std::time::SystemTime::now());
+                            match database.execute("INSERT INTO arp_replies (replier_ip, replier_mac, destination_ip, timestamp) VALUES (?1, ?2, ?3, ?4)", (&replier_ip, &replier_mac, &destination_ip, &timestamp)) {
+                                Ok(_) => {},
+                                Err(why) => {
+                                    return Err(format!("Failed to execute query: {}", why.to_string().bright_black().bold()));
+                                }
+                            };
+                            println!("{} {} is at {} (replying to {})", "ARP REPLY  ".blue(), replier_ip.bright_black().bold(), replier_mac.bright_black().bold(), destination_ip.bright_black().bold());
                         }
                     } else {
                         println!("{}: Could not parse ARP packet", "Error".red().bold());
@@ -79,7 +153,7 @@ fn main() {
             },
 
             Err(why) => {
-                println!("{}: Failed to read packet: {}", "Error".red().bold(), why.to_string().bright_black().bold());
+                return Err(format!("Failed to read packet: {}", why.to_string().bright_black().bold()));
             }
         }
     }
